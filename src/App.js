59333a7718a8cac24df924ebdfdd6dcbc55b31d6
chandan0134 <chandanma39@gmail.com>
2023-10-20 23:45:54 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Forecast from "./components/Forecast";
 export default function App() {
   return (
 
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-20 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+    <div className="mx-auto my-5 max-w-screen-md mt-4 py-5 px-20 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
     <TopButtons/>
     <Inputs/>
     <TimeAndLocation/>
